7836ab8a0c76687802632c4cd88a2c03fe3ae791
maxwellpv <55470371+maxwellpv@users.noreply.github.com>
2023-05-22 22:57:17 -0500

    Add HomeModule, CandidateComponent, EmployerComponent and NavBarComponent. Refactor "router-outlet" from app.component.html to nav-bar.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { Routes, RouterModule } from '@angular/router';
+import { EmployerComponent } from './home/employer/employer.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {MatButtonModule} from "@angular/material/button";
+import {CandidateComponent} from "./home/candidate/candidate.component";
+import {MatSidenavModule} from "@angular/material/sidenav"; // CLI imports router
 
 const routes: Routes = [
-  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)}
+  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
 
-]; 
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployerComponent,
+    CandidateComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   exports: [
-    RouterModule
+    RouterModule,
+    NavBarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
