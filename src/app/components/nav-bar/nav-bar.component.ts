a21b1a8641b381953b4c594d4514552f66a11ea1
maxwellpv <55470371+maxwellpv@users.noreply.github.com>
2023-06-03 17:51:07 -0500

    Refactor components and services

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -5,6 +5,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit{
+  username: string = "Test";
   events: string[] = [];
   opened: boolean = false;
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {EmployerComponent} from "./employer/employer.component";
 import { CandidateComponent } from './candidate/candidate.component';
+import {JobOfferViewComponent} from "../components/job-offer/job-offer-view/job-offer-view.component";
 
 const routes: Routes = [
   { path: 'candidate', component: CandidateComponent },
-  { path: 'employer', component: EmployerComponent}
+  { path: 'employer', component: EmployerComponent},
+  { path: 'job-offer-view/:id', component: JobOfferViewComponent}
 ]; // sets up routes constant where you define your routes
 
 @NgModule({
