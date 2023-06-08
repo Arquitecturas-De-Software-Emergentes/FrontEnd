bd1725691d8bef6146e56f439332b3f7aa80bafb
RicardoYalico <u201920140@upc.edu.pe>
2023-06-07 20:03:57 -0500

    Vista de los perfiles postulante y empleador

--- src/app/profile/resume-profile/resume-profile.component.ts
+++ src/app/profile/resume-profile/resume-profile.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-resume-profile',
   templateUrl: './resume-profile.component.html',
   styleUrls: ['./resume-profile.component.css']
 })
-export class ResumeProfileComponent {
+export class ResumeProfileComponent implements OnInit{
+  companyName = new FormControl('');
+  description = new FormControl('');
+  websiteUrl = new FormControl('');
+  profilePicture = new FormControl('');
+  bannerPicture = new FormControl('');
 
+  firstName = new FormControl('');
+  lastName = new FormControl('');
+  salary = new FormControl('');
+  email = new FormControl('');
+  phoneNumber = new FormControl('');
+  workingPlaces = new FormControl('');
+  languages = new FormControl('');
+  skills = new FormControl('');
+  studyCenter = new FormControl('');
+  about = new FormControl('');
+
+
+
+
+  loading = false;
+  role: number = 0;
+
+  ngOnInit(): void {
+    this.role = Number(sessionStorage.getItem('role'));
+    console.log(this.role)
+  }
 }
